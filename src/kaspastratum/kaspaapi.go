3db6282fbf76a1dfcd53b6dbe380e594854176a6
Alirezazaktab110 <114968361+Alirezazaktab110@users.noreply.github.com>
2023-12-13 13:40:58 +0330

    Update kaspaapi.go

--- src/kaspastratum/kaspaapi.go
+++ src/kaspastratum/kaspaapi.go
@@ -5,14 +5,14 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/kaspanet/kaspad/app/appmessage"
-	"github.com/kaspanet/kaspad/infrastructure/network/rpcclient"
+	"github.com/Pyrinpyi/pyipad/app/appmessage"
+	"github.com/Pyrinpyi/pyipad/infrastructure/network/rpcclient"
 	"github.com/onemorebsmith/kaspastratum/src/gostratum"
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
 )
 
-type KaspaApi struct {
+type PyrinApi struct {
 	address       string
 	blockWaitTime time.Duration
 	logger        *zap.SugaredLogger
@@ -20,13 +20,13 @@ type KaspaApi struct {
 	connected     bool
 }
 
-func NewKaspaAPI(address string, blockWaitTime time.Duration, logger *zap.SugaredLogger) (*KaspaApi, error) {
+func NewPyrinAPI(address string, blockWaitTime time.Duration, logger *zap.SugaredLogger) (*PyrinApi, error) {
 	client, err := rpcclient.NewRPCClient(address)
 	if err != nil {
 		return nil, err
 	}
 
-	return &KaspaApi{
+	return &PyrinApi{
 		address:       address,
 		blockWaitTime: blockWaitTime,
 		logger:        logger.With(zap.String("component", "kaspaapi:"+address)),
@@ -35,13 +35,13 @@ func NewKaspaAPI(address string, blockWaitTime time.Duration, logger *zap.Sugare
 	}, nil
 }
 
-func (ks *KaspaApi) Start(ctx context.Context, blockCb func()) {
+func (ks *PyrinApi) Start(ctx context.Context, blockCb func()) {
 	ks.waitForSync(true)
 	go ks.startBlockTemplateListener(ctx, blockCb)
 	go ks.startStatsThread(ctx)
 }
 
-func (ks *KaspaApi) startStatsThread(ctx context.Context) {
+func (ks *PyrinApi) startStatsThread(ctx context.Context) {
 	ticker := time.NewTicker(30 * time.Second)
 	for {
 		select {
@@ -49,14 +49,14 @@ func (ks *KaspaApi) startStatsThread(ctx context.Context) {
 			ks.logger.Warn("context cancelled, stopping stats thread")
 			return
 		case <-ticker.C:
-			dagResponse, err := ks.kaspad.GetBlockDAGInfo()
+			dagResponse, err := ks.pyipad.GetBlockDAGInfo()
 			if err != nil {
-				ks.logger.Warn("failed to get network hashrate from kaspa, prom stats will be out of date", zap.Error(err))
+				ks.logger.Warn("failed to get network hashrate from pyrin, prom stats will be out of date", zap.Error(err))
 				continue
 			}
-			response, err := ks.kaspad.EstimateNetworkHashesPerSecond(dagResponse.TipHashes[0], 1000)
+			response, err := ks.pyipad.EstimateNetworkHashesPerSecond(dagResponse.TipHashes[0], 1000)
 			if err != nil {
-				ks.logger.Warn("failed to get network hashrate from kaspa, prom stats will be out of date", zap.Error(err))
+				ks.logger.Warn("failed to get network hashrate from pyrin, prom stats will be out of date", zap.Error(err))
 				continue
 			}
 			RecordNetworkStats(response.NetworkHashesPerSecond, dagResponse.BlockCount, dagResponse.Difficulty)
@@ -64,47 +64,47 @@ func (ks *KaspaApi) startStatsThread(ctx context.Context) {
 	}
 }
 
-func (ks *KaspaApi) reconnect() error {
-	if ks.kaspad != nil {
-		return ks.kaspad.Reconnect()
+func (ks *pyrinApi) reconnect() error {
+	if ks.pyipad != nil {
+		return ks.pyipad.Reconnect()
 	}
 
 	client, err := rpcclient.NewRPCClient(ks.address)
 	if err != nil {
 		return err
 	}
-	ks.kaspad = client
+	ks.pyipad = client
 	return nil
 }
 
-func (s *KaspaApi) waitForSync(verbose bool) error {
+func (s *PyrinApi) waitForSync(verbose bool) error {
 	if verbose {
 		s.logger.Info("checking kaspad sync state")
 	}
 	for {
-		clientInfo, err := s.kaspad.GetInfo()
+		clientInfo, err := s.pyipad.GetInfo()
 		if err != nil {
-			return errors.Wrapf(err, "error fetching server info from kaspad @ %s", s.address)
+			return errors.Wrapf(err, "error fetching server info from pyipad @ %s", s.address)
 		}
 		if clientInfo.IsSynced {
 			break
 		}
-		s.logger.Warn("Kaspa is not synced, waiting for sync before starting bridge")
+		s.logger.Warn("Pyrin is not synced, waiting for sync before starting bridge")
 		time.Sleep(5 * time.Second)
 	}
 	if verbose {
-		s.logger.Info("kaspad synced, starting server")
+		s.logger.Info("pyipad synced, starting server")
 	}
 	return nil
 }
 
-func (s *KaspaApi) startBlockTemplateListener(ctx context.Context, blockReadyCb func()) {
+func (s *PyrinApi) startBlockTemplateListener(ctx context.Context, blockReadyCb func()) {
 	blockReadyChan := make(chan bool)
 	err := s.kaspad.RegisterForNewBlockTemplateNotifications(func(_ *appmessage.NewBlockTemplateNotificationMessage) {
 		blockReadyChan <- true
 	})
 	if err != nil {
-		s.logger.Error("fatal: failed to register for block notifications from kaspa")
+		s.logger.Error("fatal: failed to register for block notifications from pyrin")
 	}
 
 	ticker := time.NewTicker(s.blockWaitTime)
@@ -129,9 +129,9 @@ func (s *KaspaApi) startBlockTemplateListener(ctx context.Context, blockReadyCb 
 	}
 }
 
-func (ks *KaspaApi) GetBlockTemplate(
+func (ks *pyrinApi) GetBlockTemplate(
 	client *gostratum.StratumContext) (*appmessage.GetBlockTemplateResponseMessage, error) {
-	template, err := ks.kaspad.GetBlockTemplate(client.WalletAddr,
+	template, err := ks.pyipad.GetBlockTemplate(client.WalletAddr,
 		fmt.Sprintf(`'%s' via onemorebsmith/kaspa-stratum-bridge_%s`, client.RemoteApp, version))
 	if err != nil {
 		return nil, errors.Wrap(err, "failed fetching new block template from kaspa")
